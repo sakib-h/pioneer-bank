5ef7ccade73fe1ce557cca058d19d72ed717e259
Sakib Hasan <sakib100.sa@gmail.com>
2021-11-04 11:37:23 +0600

    deposit part done

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -7,7 +7,7 @@ loginBtn.addEventListener("click", function () {
 })
 
 let depositBtn = document.getElementById("deposit")
-depositBtn.addEventListener("click", function () {
+depositBtn.addEventListener("click", function (deposit) {
 
   let depositAmount = document.getElementById("deposit-amount").value;
   let depositNumber = parseFloat(depositAmount);
@@ -19,6 +19,19 @@ depositBtn.addEventListener("click", function () {
   let totalDeposit = currentDepositNumber + depositNumber;
 
   document.getElementById("current-deposit").innerText = totalDeposit;
+
+  document.getElementById("deposit-amount").value = "";
+
+  let currentBalance = document.getElementById("balance").innerText;
+  let balanceNumber = parseFloat(currentBalance);
+
+  let newBalance = totalDeposit + balanceNumber;
+
+  document.getElementById("balance").innerText = newBalance;
+
+
+
+
 })
 
 
